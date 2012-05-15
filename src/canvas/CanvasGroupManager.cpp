9647d3bd7821bb1d014ad38d06eb9c7fdbe39e61
Christopher Baker <me@christopherbaker.net>
2012-05-15 15:11:36 -0400

    Updating, deleting, moving around.

--- src/canvas/CanvasGroupManager.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  CanvasGroupManager.cpp
-//  livedraw
-//
-//  Created by Christopher P. Baker on 5/17/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
-
-#include "CanvasGroupManager.h"
--- src/sources/SharedImageSource.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  SharedFrameSource.cpp
-//  livedraw
-//
-//  Created by Christopher P. Baker on 4/14/11.
-//  Copyright 2011 Murmur Labs LLC. All rights reserved.
-//
-
-#include "SharedImageSource.h"
