e7dfb4634965c9bb43eb77410d6fb2e1934d271e
Christopher Baker <me@christopherbaker.net>
2012-07-11 17:20:10 -0500

    A special wrapper for ofxSyphon to allow pixel readback and sharing.

--- src/assets/syphon/ofxSyphonVideoClient.h
+++ src/assets/syphon/ofxSyphonVideoClient.h
@@ -8,11 +8,60 @@
 
 #pragma once
 
-class SyphonPixelClient {
-public:
-    SyphonPixelClient();
-    virtual ~SyphonPixelClient();
-protected:
+#include "ofxSyphonClient.h"
 
-private:
+class ofxSyphonVideoClient : public ofxSyphonClient, 
+                             public ofBaseVideo
+
+{
+public:
+    ofxSyphonVideoClient() {}
+    virtual ~ofxSyphonVideoClient() {}
+    
+    void update() {
+    }
+    
+    unsigned char* getPixels() {
+        if(isFrameNew()) {
+            fillPixels();
+        }
+        return pixels.getPixels();
+    }
+    
+	virtual ofPixelsRef getPixelsRef() {
+        if(isFrameNew()) {
+            fillPixels();
+        }
+        return pixels;
+    }
+
+    virtual bool isFrameNew() {
+        // TODO: no way to check this just yet
+        return true;
+    }
+    
+    void open() {
+        // TODO: not implemented
+    }
+    
+    void close() {
+        // TODO: not implemented
+    }
+
+    bool isConnected() {
+        // TODO: not implemented
+        return true;
+    }
+    
+protected:
+    void fillPixels() {
+        // TODO: this should only be called as needed
+        // there is likely no reason to do a readback here.  
+        // probably can just bind to the texture directly.
+        // but we need to bring it into the shared pointer 
+        mTex.readToPixels(pixels);
+    }
+    
+    ofPixels pixels;
+
 };
