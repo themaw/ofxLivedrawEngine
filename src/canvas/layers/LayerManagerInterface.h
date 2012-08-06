d69eec12f4440b1d1f2e7ff15731efdbe201c9d3
Christopher Baker <me@christopherbaker.net>
2012-08-06 12:19:27 -0500

    Updating names and adding layer manager interface to break circular dependencies.

---
+++ src/canvas/layers/LayerManagerInterface.h
@@ -0,0 +1,14 @@
+//
+//  CanvasLayerManagerInterface.h
+//  ofxLivedrawEngineExample
+//
+//  Created by Christopher P. Baker on 8/3/12.
+//
+//
+
+#ifndef ofxLivedrawEngineExample_CanvasLayerManagerInterface_h
+#define ofxLivedrawEngineExample_CanvasLayerManagerInterface_h
+
+
+
+#endif
