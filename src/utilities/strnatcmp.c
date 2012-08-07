c87d28ffb843a4f5ac15826f5ecf6bf5e04cf37e
Christopher Baker <me@christopherbaker.net>
2012-08-07 02:28:37 -0500

    Added natural string ordering class from Dirk Jagdmann <doj@cubic.org>.

---
+++ src/utilities/strnatcmp.c
@@ -0,0 +1,14 @@
+//
+//  strnatcmp.c
+//  ofxLivedrawEngineExample
+//
+//  Created by Christopher P. Baker on 8/6/12.
+//
+//
+
+#ifndef ofxLivedrawEngineExample_strnatcmp_c
+#define ofxLivedrawEngineExample_strnatcmp_c
+
+
+
+#endif
---
+++ src/utilities/strnatcmp.h
@@ -0,0 +1,14 @@
+//
+//  strnatcmp.h
+//  ofxLivedrawEngineExample
+//
+//  Created by Christopher P. Baker on 8/6/12.
+//
+//
+
+#ifndef ofxLivedrawEngineExample_strnatcmp_h
+#define ofxLivedrawEngineExample_strnatcmp_h
+
+
+
+#endif
