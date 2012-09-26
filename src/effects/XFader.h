40b6bdbb015f89c9d03de11625667c387dff3798
Christopher Baker <me@christopherbaker.net>
2012-09-25 19:05:51 -0500

    New xfade shader (in progress)

--- src/effects/XFader.h
+++ src/effects/XFader.h
@@ -1,14 +1,46 @@
-//
-//  XFader.h
-//  ofxLivedrawEngineExample
-//
-//  Created by Christopher P. Baker on 9/24/12.
-//
-//
+/*==============================================================================
+ 
+ Copyright (c) 2010, 2011, 2012 Christopher Baker <http://christopherbaker.net>
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ 
+ ==============================================================================*/
 
-#ifndef ofxLivedrawEngineExample_XFader_h
-#define ofxLivedrawEngineExample_XFader_h
+#pragma once
 
+#include "ofMain.h"
 
-
-#endif
+class XFader : public ofShader {
+    
+public:
+    XFader() : ofShader() {}
+    ~XFader() {};
+    
+    void setup() {
+        setupShaderFromFile(GL_FRAGMENT_SHADER, "application/shaders/maw.shader.xfader.frag");
+        linkProgram();
+    }
+    
+    void apply(ofTexture& input, ofTexture& mask, float xfade) {
+        setUniformTexture("sourceTexture", input, 1 );
+        setUniformTexture("maskTexture", mask, 2);
+        
+        setUniform1i("xfade",   xfade);
+    }
+};
